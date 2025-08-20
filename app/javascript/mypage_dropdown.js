c6dc4deb77c75ccb3885847e6e1fb5013f1f0f63
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-21 04:51:16 +0900

    コメント追加

--- app/javascript/mypage_dropdown.js
+++ app/javascript/mypage_dropdown.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let hideTimeout;
+  let hideTimeout; // タイマーを管理する変数
 
   const dropdownContainer = document.getElementById("dropdown-container");
   const dropdownMenu = document.getElementById("dropdown-menu");
 
   dropdownContainer.addEventListener("mouseenter", () => {
+    // 非表示のタイマーがある場合はキャンセル
     if (hideTimeout) {
       clearTimeout(hideTimeout);
       hideTimeout = null;
     }
-
+    // メニューを表示
     dropdownMenu.style.display = "block";
   });
 
   dropdownContainer.addEventListener("mouseleave", () => {
+    // 少し遅延してからメニューを非表示にする
     hideTimeout = setTimeout(() => {
       dropdownMenu.style.display = "none";
     }, 200);
