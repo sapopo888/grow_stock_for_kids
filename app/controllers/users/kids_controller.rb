8697610428a4f3f21c8367f9965faa5a492cbfef
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-22 00:34:39 +0900

    子供登録機能追加

--- app/controllers/users/kids_controller.rb
+++ app/controllers/users/kids_controller.rb
@@ -2,7 +2,7 @@ class Users::KidsController < ApplicationController
    before_action :authenticate_user!
 
   def index
-    # @kids = current_user.kids.find(params[:id])
+    @kids = current_user.kid.find(params[:id])
   end
 
   def new
@@ -10,13 +10,19 @@ class Users::KidsController < ApplicationController
   end
 
   def create
-    @kid = Kid.new
-    if save?
+    @kid = current_user.kid.build(kid_params)
+    if @kid.save
       flash[:notice] = '登録に成功しました'
       redirect_to kids_path
     else
       flash.now[:alert] = '登録に失敗しました'
       render :new
     end
+  end
+
+  private
+
+  def kid_params
+    params.require(:kid).permit( :name )
   end
 end
