65396ce25ff1174bf31c315276c1c3d8b9e89f39
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-25 09:50:43 +0900

    ログイン後dashboards/index.html.erbに遷移

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -20,7 +20,11 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+    def after_sign_in_path_for(resource)
+     admins_dashboards_index_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
