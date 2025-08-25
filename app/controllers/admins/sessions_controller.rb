6aa1dfb63362fa85bb1d386ac13251c1845f2e0b
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-25 14:07:22 +0900

    adminログアウト後、ログイン画面に遷移

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -22,8 +22,12 @@ class Admins::SessionsController < Devise::SessionsController
 
   protected
 
-    def after_sign_in_path_for(resource)
-     admins_dashboards_index_path
+  def after_sign_in_path_for(resource)
+    admins_dashboards_index_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
   end
 
   # If you have extra params to permit, append them to the sanitizer.
