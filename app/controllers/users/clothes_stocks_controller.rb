0ec7783768918242b7cdb4d992822b2aa962d0ff
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-29 08:30:13 +0900

    kidをupdateできるようにeditページのform_withのurlをusers_kid_pathに修正

--- app/controllers/users/clothes_stocks_controller.rb
+++ app/controllers/users/clothes_stocks_controller.rb
@@ -1,2 +1,14 @@
 class Users::ClothesStocksController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @clothes = StockClothes.new
+  end
+
+  def create
+    @clothes = StockClothes.new(stock_clothes_params)
+    if @clothes.save
+      redirect_to 
+    end
+  end
 end
