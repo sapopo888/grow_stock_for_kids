e4f0c964b74db81c1844856a00d4183c5d296263
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-28 23:37:35 +0900

    ルーティング・コントローラ記載、Category新規作成機能追加

--- app/controllers/admins/categories_controller.rb
+++ app/controllers/admins/categories_controller.rb
@@ -1,2 +1,30 @@
 class Admins::CategoriesController < Admins::BaseController
+  before_action :set_category, only: %i[]
+
+  def index
+    @categories = Category.all.order(created_at: :asc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admins_categories_path, notice: "Categoryを作成しました", status: :see_other
+    else
+      render :new, alert: 'Categoryの作成に失敗しました', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
