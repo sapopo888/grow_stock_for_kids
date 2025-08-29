55e86fe5211bca25dec65a3e36ae5e534592caa5
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-29 09:20:35 +0900

    clotes_stockのnew機能一時保存用

--- app/controllers/users/clothes_stocks_controller.rb
+++ app/controllers/users/clothes_stocks_controller.rb
@@ -1,14 +1,26 @@
 class Users::ClothesStocksController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @clothes_stocks = current_user.clothes_stock.order(created_at: :desc)
+  end
+
   def new
-    @clothes = StockClothes.new
+    @clothes_stock = ClothesStock.new
   end
 
   def create
-    @clothes = StockClothes.new(stock_clothes_params)
-    if @clothes.save
-      redirect_to 
+    @clothes_stock = ClothesStock.new(clothes_stock_params)
+    if @clothes_stock.save
+      redirect_to users_clothes_stocks_path, notice: 'ストックを作成しました', status: :see_other
+    else 
+      render :new, alert: 'ストックの作成に失敗しました', status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def clothes_stock_params
+    params.require(:clothes_stock).permit(:comment)
   end
 end
