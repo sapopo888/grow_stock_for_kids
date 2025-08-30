7aa8c07abb1aa8e80b3527167a73dab1870c804b
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-30 21:51:46 +0900

    clothes_stocksコントローラ記述、日本語化

--- app/controllers/users/clothes_stocks_controller.rb
+++ app/controllers/users/clothes_stocks_controller.rb
@@ -1,5 +1,6 @@
 class Users::ClothesStocksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_clothes_stock, only: %i[show edit update destroy]
 
   def index
     @clothes_stocks = current_user.clothes_stock.order(created_at: :desc)
@@ -15,13 +16,34 @@ class Users::ClothesStocksController < ApplicationController
   def create
     @clothes_stock = ClothesStock.new(clothes_stock_params)
     if @clothes_stock.save
-      redirect_to users_clothes_stocks_path, notice: "ストックを作成しました", status: :see_other
+      redirect_to users_clothes_stocks_path, notice: t("defaults.flash_message.registered", item: ClothesStock.model_name.human), status: :see_other
     else
-      render :new, alert: "ストックの作成に失敗しました", status: :unprocessable_entity
+      render :new, alert: t("defaults.flash_message.not_registered", item: ClothesStock.model_name.human), status: :unprocessable_entity
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    if @clothes_stock.update(clothes_stock_params)
+      redirect_to users_clothes_stocks_path, notice: t("defaults.flash_message.updated", item: ClothesStock.model_name.human), status: :see_other
+    else
+      render :edit, alert: t("defaults.flash_message.not_updated", item: ClothesStock.model_name.human), status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @clothes_stock.destroy!
+    redirect_to users_clothes_stock_path, notice: t("defaults.flash_message.deleted", item: ClothesStock.model_name.human)
+  end
+
   private
+
+  def set_clothes_stock
+    @clothes_stock = current_user.clothes_stock.find(params[:id])
+  end
 
   def clothes_stock_params
     params.require(:clothes_stock).permit(
