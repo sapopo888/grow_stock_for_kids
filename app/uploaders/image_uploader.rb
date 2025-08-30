8ba22e827850fcacc7c22ebb07b26f25a2e103ca
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-30 14:29:34 +0900

    画像のアップロード機能追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,12 +15,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*args)
+    'https://placehold.jp/150x150.png'
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
