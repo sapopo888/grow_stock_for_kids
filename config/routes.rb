d7f0d80e67ad04484d386a3c1a15a85c4ca46fde
sapopo888 <sapopo0813@yahoo.co.jp>
2025-09-03 23:10:08 +0900

    gem letter_opener_webをbundle install, 各種設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :admins, controllers: {
     sessions: "admins/sessions",
     registrations: "admins/registrations"
