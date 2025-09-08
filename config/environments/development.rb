9e858fe2a898e758bc7d85228bf9300559d90854
sapopo888 <sapopo0813@yahoo.co.jp>
2025-09-08 22:32:30 +0900

    rubocop修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,12 +45,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: "smtp.gmail.com",
     port: 587,
-    domain: 'location',
-    user_name: ENV['MAILER_SENDER'],
-    password: ENV['MAILER_PASSWORD'],
-    authentication: 'plain',
+    domain: "location",
+    user_name: ENV["MAILER_SENDER"],
+    password: ENV["MAILER_PASSWORD"],
+    authentication: "plain",
     enable_starttls_auto: true
   }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,15 +81,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'https://grow-stock-for-kids.onrender.com/' }
+  config.action_mailer.default_url_options = { host: "https://grow-stock-for-kids.onrender.com/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: "smtp.gmail.com",
     port: 587,
-    domain: 'grow-stock-for-kids.onrender.com',
-    user_name: ENV['MAILER_SENDER'],
-    password: ENV['MAILER_PASSWORD'],
-    authentication: 'plain',
+    domain: "grow-stock-for-kids.onrender.com",
+    user_name: ENV["MAILER_SENDER"],
+    password: ENV["MAILER_PASSWORD"],
+    authentication: "plain",
     enable_starttls_auto: true
   }
 
