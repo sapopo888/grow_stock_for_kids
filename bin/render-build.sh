a9fb68f7f6abd9cf4d9c476d499826b86b553d28
sapopo888 <sapopo0813@yahoo.co.jp>
2025-09-14 19:34:47 +0900

    本番環境でエラー解決し動作確認できたのでrender-build.sh元に戻す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rails db:migrate ←コメントアウト
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset # ←追記
+bundle exec rails db:migrate
