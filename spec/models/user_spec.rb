6aadc4097a229c6b90c41b1b54426fc469b7abe7
sapopo888 <sapopo0813@yahoo.co.jp>
2025-09-15 06:54:44 +0900

    factorybotでのテスト通過確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'バリデーションチェック' do
     it '設定したすべてのバリデーションが機能しているか' do
-      user = User.new(name: "ユーザー", status: 0, email: "user@example.com", password: "password", password_confirmation: "password")
+      user = build(:user) # FactoryBot.build(:user)と同じ
       expect(user).to be_valid
       expect(user.errors).to be_empty
     end
 
     it 'nameが空の時にバリデーションが通らない' do
-      user = User.new(name: "", status: 0, email: "user@example.com", password: "password", password_confirmation: "password")
+      user = build(:user, name: "")
       expect(user).to be_invalid
       expect(user.errors).not_to be_empty
     end
 
     it 'statusが空の時にバリデーションが通らない' do
-      user = User.new(name: "ユーザー", status: nil, email: "user@example.com", password: "password", password_confirmation: "password")
+      user = build(:user, status: nil)
       expect(user).to be_invalid
       expect(user.errors).not_to be_empty
     end
