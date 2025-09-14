9509846e46ec144538c3d254ffb231f277c18d2d
sapopo888 <sapopo0813@yahoo.co.jp>
2025-09-15 06:29:46 +0900

    Userモデルのバリデーションのテストコード記述、テスト通過確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it '設定したすべてのバリデーションが機能しているか' do
+      user = User.new(name: "ユーザー", status: 0, email: "user@example.com", password: "password", password_confirmation: "password")
+      expect(user).to be_valid
+      expect(user.errors).to be_empty
+    end
+
+    it 'nameが空の時にバリデーションが通らない' do
+      user = User.new(name: "", status: 0, email: "user@example.com", password: "password", password_confirmation: "password")
+      expect(user).to be_invalid
+      expect(user.errors).not_to be_empty
+    end
+
+    it 'statusが空の時にバリデーションが通らない' do
+      user = User.new(name: "ユーザー", status: nil, email: "user@example.com", password: "password", password_confirmation: "password")
+      expect(user).to be_invalid
+      expect(user.errors).not_to be_empty
+    end
+  end
 end
