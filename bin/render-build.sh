2ae82c64f9ca3ce4523aaa27efe429ffcde153b4
sapopo888 <sapopo0813@yahoo.co.jp>
2025-09-14 19:05:02 +0900

    renderのエラー対処のためデータベース一旦リセット

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+# bundle exec rails db:migrate ←コメントアウト
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec db:migrate:reset # ←追記
